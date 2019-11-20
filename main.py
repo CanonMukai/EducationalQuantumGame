b33af676fcff594b73ac11aff4650c9f1042402d
向井かのん <canon@mukaikanonnoMacBook-Pro.local>
2019-11-20 11:21:39 +0900

    11:21

--- main.py
+++ main.py
@@ -94,12 +94,11 @@ def solve_problem(tele):
    return answer
 
 def evaluate(question, answer):
-    if question[0] == "X":
-        correct_answer = x_gate(question[1])
-        if correct_answer == answer:
-            return True
-        else:
-            return False
+   correct_answer = x_gate(question)
+   if correct_answer == answer:
+      return True
+   else:
+      return False
 
 def CircleOrBatsu(is_correct, screen):
    if is_correct:
@@ -107,6 +106,7 @@ def CircleOrBatsu(is_correct, screen):
    else:
        pygame.draw.line(screen, (0, 0, 255), (825, 115), (1125, 415), 50)
        pygame.draw.line(screen, (0, 0, 255), (825, 415), (1125, 115), 50)
+   time.sleep(1)
    pygame.display.update()
    time.sleep(1)
 
@@ -168,8 +168,8 @@ class Transparent():
        screen.blit(s, (self.x0 - self.width + shift, self.y0))
 
 class Underbar():
-    def __init__(self, x, y):
-        self.sysfont = pygame.font.SysFont(None, 70)
+    def __init__(self, x, y, fontsize):
+        self.sysfont = pygame.font.SysFont(None, fontsize)
         self.underbar = "_"
         (self.x, self.y) = (x, y)
     def draw(self, screen):
@@ -179,7 +179,7 @@ class Underbar():
 
 class Input():
     def __init__(self, x, y, answer):
-        self.sysfont = pygame.font.SysFont(None, 70)
+        self.sysfont = pygame.font.SysFont(None, 60)
         if answer == 0:
             self.input = 0
         else:
@@ -188,7 +188,38 @@ class Input():
     def draw(self, screen):
         img = self.sysfont.render(str(self.input), True, (20,20,20))
         screen.blit(img, (self.x, self.y))
+        pygame.display.update()
 
+def hadamard_gate1(A):
+   q = QuantumRegister(1)
+   c = ClassicalRegister(1)
+   qc = QuantumCircuit(q,c)
+   inputdata = [A]
+   if inputdata[0] == 1:
+       qc.x(q[0])
+   qc.h(q[0])
+   backend = BasicAer.get_backend('statevector_simulator')
+   job = execute(qc, backend)
+   result = job.result()
+   outputstate = result.get_statevector(qc, decimals=3)
+   answer_a = outputstate[0]
+   answer_b = outputstate[1]
+   dict = {}
+   if answer_a == 0.707+ 0j:
+       dict["0"] =  "+"
+   if answer_b == -0.707 + 0j:
+       dict["1"] = "-"
+   else:
+       dict["1"] = "+"
+   return dict
+
+def evaluateH(qubit, answer_dict):
+   print ("correct_dict = %s" % hadamard_gate1(qubit))
+   print ("answer_dict = %s" % answer_dict)
+   if hadamard_gate1(qubit) == answer_dict:
+      return True
+   else:
+      return False
 
 def main():
     pygame.init()
@@ -218,15 +249,18 @@ def main():
                     [[["H", 0]], 1, [[2]], [["+","+"]], ["h_gate0.png"], "h0_circ.png"],
                     [[["H", 1]], 1, [[2]], [["+","-"]], ["h_gate1.png"], "h0_circ.png"],
                     [[["X",0],["H",1],["CX",1,2],["CX",0,1],["H",0],["CX",1,2],["CZ",0,2]], 3,
-                    [1, 2, 2, 2, 4, 4, 4, 4],
+                    [1, 2, 2, 2, 4, 4, 4],
                     [["+"], ["+", "+"], ["+", "+"], ["+", "+"], ["+", "-", "+", "-"], ["+", "-", "+", "-"], ["+", "+", "+", "+"]],
                     ["teleportation0.png", "tereportation123.png", "tereportation123.png", "tereportation123.png", "tereportation45.png", "tereportation45.png", "teleportation6.png"], "teleportation_circuit.png"]
                     ]
     #question_list = ["x_gate0.png", "x_gate1.png", "h_gate0.png", "h_gate1.png"]
     #choose question randomly
     question_num = random.randint(0, len(question_list) - 1)
-    question_num = 4
-    question = Picture(question_list[question_num][4][0], 820, 150)
+    question_num = 0
+    step = 6
+    if question_num != 4:
+        step = 0
+    question = Picture(question_list[question_num][4][step], 820, 150)
     circuit = Picture(question_list[question_num][5],150, 520)
     transparent = Transparent(670,530,370,200,100)
     #dict1 = {"00":"+", "01":"-"}
@@ -234,8 +268,51 @@ def main():
     if question_num == 4 :
         for i in range(len(question_list[question_num][0])):
     """
-    underbar = Underbar(998, 200)
-    underbar2 = Underbar(1086, 200)
+    #アンダーバーの座標とサイズを設定  Underbar(x座標, y座標, フォントサイズ)
+    if question_num == 0 or question_num == 1:
+        underbar = Underbar(998, 200, 70)
+    if question_num == 2 or question_num == 3:
+        underbar = Underbar(1002, 200, 70)
+        underbar2 = Underbar(1086, 200, 70)
+    if question_num == 4:
+        #k = question_list[question_num][1] * question_list[question_num][2][step]
+        #for i in range(k):
+
+        if step == 0:
+            underbar = Underbar(820, 200, 60)
+            underbar2 = Underbar(850, 200, 60)
+            underbar3 = Underbar(880, 200, 60)
+        if step == 1 or step == 2 or step == 3:
+            #一つ目の枠
+            underbar = Underbar(888, 200, 40)
+            underbar2 = Underbar(908, 200, 40)
+            underbar3 = Underbar(928, 200, 40)
+            #二つ目の枠
+            underbar4 = Underbar(1005, 200, 40)
+            underbar5 = Underbar(1025, 200, 40)
+            underbar6 = Underbar(1045, 200, 40)
+        #tereportation45.pngのheightを30に設定
+        if step == 4 or step == 5 or step == 6 or step == 7:
+            #一つ目の枠
+            underbar = Underbar(850, 200, 30)
+            underbar2 = Underbar(864, 200, 30)
+            underbar3 = Underbar(878, 200, 30)
+            #二つ目の枠
+            underbar4 = Underbar(932, 200, 30)
+            underbar5 = Underbar(946, 200, 30)
+            underbar6 = Underbar(960, 200, 30)
+            #三つ目の枠
+            underbar7 = Underbar(1014, 200, 30)
+            underbar8 = Underbar(1028, 200, 30)
+            underbar9 = Underbar(1042, 200, 30)
+            #四つ目の枠
+            underbar10 = Underbar(1096, 200, 30)
+            underbar11 = Underbar(1110, 200, 30)
+            underbar12 = Underbar(1124, 200, 30)
+
+    count = 0
+    answer_dict = {}
+    input1 = []
 
     while True:
         screen.fill((255, 255, 255))
@@ -251,40 +328,151 @@ def main():
         #answer_box.draw(screen, answer_number)
         question.draw(screen)
         circuit.draw(screen)
-        underbar.draw(screen)
-        underbar2.draw(screen)
-        transparent.draw(screen,48)
-        pygame.display.update()
+        #アンダーバーを表示
+        if question_num == 0 or question_num == 1:
+            underbar.draw(screen)
+        if question_num == 2 or question_num == 3:
+            underbar.draw(screen)
+            underbar2.draw(screen)
+        if question_num == 4:
+            if step >= 4:
+                transparent.draw(screen,48*(step+1))
+            else:
+                transparent.draw(screen,48*step)
+            if step == 0:
+                underbar.draw(screen)
+                underbar2.draw(screen)
+                underbar3.draw(screen)
+            if step == 1 or step == 2 or step == 3:
+                underbar.draw(screen)
+                underbar2.draw(screen)
+                underbar3.draw(screen)
+                underbar4.draw(screen)
+                underbar5.draw(screen)
+                underbar6.draw(screen)
+            if step == 4 or step == 5 or step == 6 or step == 7:
+                underbar.draw(screen)
+                underbar2.draw(screen)
+                underbar3.draw(screen)
+                underbar4.draw(screen)
+                underbar5.draw(screen)
+                underbar6.draw(screen)
+                underbar7.draw(screen)
+                underbar8.draw(screen)
+                underbar9.draw(screen)
+                underbar10.draw(screen)
+                underbar11.draw(screen)
+                underbar12.draw(screen)
+
+        #underbar.draw(screen)
+        #underbar2.draw(screen)
+        #サーキットを隠す半透明
+        # transparent.draw(screen,48)
+        # pygame.display.update()
 
         for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if button0.collidepoint(event.pos):
-                    answer = 0
-                    input = Input(1000, 200, answer)
-                    input.draw(screen)
-                    is_correct = evaluate(questions[question_number], answer)
-                    CircleOrBatsu(is_correct, screen)
-                    if is_correct:
-                        print ("correct")
-                        score.add_score(10)
+           if event.type == QUIT:
+               pygame.quit()
+               sys.exit()
+           if event.type == pygame.MOUSEBUTTONDOWN:
+               if question_num == 0 or question_num == 1:
+                   if button0.collidepoint(event.pos):
+                       answer = 0
+                       input = Input(1000, 200, answer)
+                       input.draw(screen)
+                       is_correct = evaluate(question_list[question_num][0][0][1], answer)
+                       CircleOrBatsu(is_correct, screen)
+                       if is_correct:
+                           print ("correct")
+                           score.add_score(10)
+                       else:
+                           print ("incorrect")
+                   if button1.collidepoint(event.pos):
+                       answer = 1
+                       input = Input(1000, 200, answer)
+                       input.draw(screen)
+                       is_correct = evaluate(question_list[question_num][0][0][1], answer)
+                       CircleOrBatsu(is_correct, screen)
+                       if is_correct:
+                          print ("correct")
+                          score.add_score(10)
+                       else:
+                          print ("incorrect")
+               if question_num == 2 or question_num == 3:
+                   if button0.collidepoint(event.pos):
+                       answer = 0
+                       if count < 2:
+                          input1.append(Input(1000 + 100 * count, 200, answer))
+                          input1[count].draw(screen)
+                          pygame.display.update()
+                          print (count)
+                          answer_dict[str(answer)] = question_list[question_num][3][0][count]
+                          print (answer_dict)
+                          if count == 1:
+                             is_correct = evaluateH(question_list[question_num][0][0][1], answer_dict)
+                             CircleOrBatsu(is_correct, screen)
+                             if is_correct:
+                                print ("correct")
+                                score.add_score(10)
+                             else:
+                                print ("incorrect")
+                             count = 0
+                             answer_dict = {}
+                             break
+                          count += 1
+                   if button1.collidepoint(event.pos):
+                       answer = 1
+                       if count < 2:
+                          input1.append(Input(1000 + 100 * count, 200, answer))
+                          input1[count].draw(screen)
+                          pygame.display.update()
+                          print (count)
+                          answer_dict[str(answer)] = question_list[question_num][3][0][count]
+                          print (answer_dict)
+                          if count == 1:
+                             is_correct = evaluateH(question_list[question_num][0][0][1], answer_dict)
+                             CircleOrBatsu(is_correct, screen)
+                             if is_correct:
+                                print ("correct")
+                                score.add_score(10)
+                             else:
+                                print ("incorrect")
+                             count = 0
+                             answer_dict = {}
+                             break
+                          count += 1
+        pygame.display.update()
 
-                    else:
-                        print ("incorrect")
-                if button1.collidepoint(event.pos):
-                    answer = 1
-                    input = Input(1000, 200, answer)
-                    input.draw(screen)
-                    is_correct = evaluate(questions[question_number], answer)
-                    CircleOrBatsu(is_correct, screen)
-                    if is_correct:
-                        print ("correct")
-                        score.add_score(10)
-
-                    else:
-                        print ("incorrect")
+        # for event in pygame.event.get():
+        #     if event.type == QUIT:
+        #         pygame.quit()
+        #         sys.exit()
+        #     if event.type == pygame.MOUSEBUTTONDOWN:
+        #         if question_num == 0 or question_num == 1:
+        #             if button0.collidepoint(event.pos):
+        #                 answer = 0
+        #                 input = Input(1000, 200, answer, 70)
+        #                 input.draw(screen)
+        #                 is_correct = evaluate(questions[question_number], answer)
+        #                 CircleOrBatsu(is_correct, screen)
+        #                 if is_correct:
+        #                     print ("correct")
+        #                     score.add_score(10)
+        #
+        #                 else:
+        #                     print ("incorrect")
+        #             if button1.collidepoint(event.pos):
+        #                 answer = 1
+        #                 input = Input(1000, 200, answer)
+        #                 input.draw(screen)
+        #                 is_correct = evaluate(questions[question_number], answer)
+        #                 CircleOrBatsu(is_correct, screen)
+        #                 if is_correct:
+        #                     print ("correct")
+        #                     score.add_score(10)
+        #
+        #                 else:
+        #                     print ("incorrect")
 
 if __name__=="__main__":
     main()
